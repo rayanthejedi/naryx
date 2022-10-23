5e3ec246f1623cbca18b818b017f846dec0ec4cd
naryx <naryx@DESKTOP-DDTGLPE>
2022-10-23 14:26:04 +0300

    CL-2022.2.4 <naryx@DESKTOP-DDTGLPE Create laf.xml, print.xml, diff.xml, toolchains.xml, main.cpp, file.template.settings.xml, find.xml, vcs.xml, main (1).cpp, editor-font.xml, colors.scheme.xml, databaseDrivers.xml, editor.xml, debugger.xml, updates.xml

---
+++ fileTemplates/main (1).cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    std::cout << "sometimes, words are not enough to express the best feelings"
+                 "but"
+                 "i wanna say that you are the best thing that ever happened to me"
+                 "and i am so proud of you no matter what and i will always love you"
+                 "thank you for  being there and for being you"<< std::endl;
+    return 0;
+}
